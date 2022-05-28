6bd53b70a8a5a4df39619bf4373b589d45fdbdf6
igorbrison <igoralexandre105@hotmail.com>
2022-05-28 16:06:38 -0300

    Calculadora
    Corrigindo problema na classe calculadora.

--- Projeto Calculadora/src/Classes/Calculadora.java
+++ Projeto Calculadora/src/Classes/Calculadora.java
@@ -1,5 +1,10 @@
 package Classes;
 
-public class Calculadora {
+public abstract class Calculadora {
+	
+	public abstract float soma(float x, float y);
+	public abstract float subtracao(float x, float y);
+	public abstract float multiplicacao(float x, float y);
+	public abstract float divisao(float x, float y);
 
 }
